dc2237256071e720e323b4be538a2f7c2dd70526
Hennessy <hennykdev@gmail.com>
2018-02-14 20:25:10 -0600

    updated splash

--- app/src/main/java/syntacticsugar/golfproject/Splash.java
+++ app/src/main/java/syntacticsugar/golfproject/Splash.java
@@ -2,15 +2,12 @@ package syntacticsugar.golfproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 /**
  * Created by Dalton on 7/10/2015.
+ * Updated by Henny 2018
  */
 public class Splash extends Activity {
 
@@ -24,12 +21,10 @@ public class Splash extends Activity {
             @Override
             public void run() {
                 Intent mainIntent = new Intent(Splash.this, MenuScreen.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
+                startActivity(mainIntent);
+                finish();
             }
         }, 2000);
+    }
 
-        
-    }//end onCreate
-
-}//end class
+}
